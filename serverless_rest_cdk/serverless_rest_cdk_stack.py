d5840d8e012dabcde05583209ad9fe41fb37e9ae
Antonio Castro <toncas@windowslive.com>
2024-06-27 13:52:59 -0500

    added Cors settings to allow all origin and methods.

--- serverless_rest_cdk/serverless_rest_cdk_stack.py
+++ serverless_rest_cdk/serverless_rest_cdk_stack.py
@@ -2,6 +2,7 @@ from aws_cdk import (
     Stack,
     aws_apigateway,
     aws_lambda,
+    aws_dynamodb,
 )
 from constructs import Construct
 
@@ -10,17 +11,30 @@ class PyRestApiStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        empl_lambda = aws_lambda.Function(
+        empl_table = aws_dynamodb.TableV2(
             self,
-            "EmplLambda",
-            runtime=aws_lambda.Runtime.PYTHON_3_11,
-            handler="index.handler",
-            code=aws_lambda.Code.from_asset("services")
+            'EmplTablePy',
+            partition_key=aws_dynamodb.Attribute(name='id', type=aws_dynamodb.AttributeType.STRING),
+            billing=aws_dynamodb.Billing.on_demand(),
         )
 
-        api = aws_apigateway.RestApi(self, "Py-Empl-Api")
-        empl_resource = api.root.add_resource("empl")
+        empl_lambda = aws_lambda.Function(
+            self,
+            'EmplLambda',
+            runtime=aws_lambda.Runtime.PYTHON_3_11,
+            handler='index.handler',
+            code=aws_lambda.Code.from_asset('services'),
+            environment={'TABLE_NAME': empl_table.table_name}
+        )
+        empl_table.grant_read_write_data(empl_lambda)
+
+        api = aws_apigateway.RestApi(self, 'Py-Empl-Api')
+        cors_options = aws_apigateway.CorsOptions(
+            allow_origins=aws_apigateway.Cors.ALL_ORIGINS,
+            allow_methods=aws_apigateway.Cors.ALL_METHODS,
+        )
+        empl_resource = api.root.add_resource('empl', default_cors_preflight_options=cors_options)
         empl_lamda_integration = aws_apigateway.LambdaIntegration(empl_lambda)
 
-        empl_resource.add_method("GET", empl_lamda_integration)
-        empl_resource.add_method("POST", empl_lamda_integration)
+        empl_resource.add_method('GET', empl_lamda_integration)
+        empl_resource.add_method('POST', empl_lamda_integration)
--- services/index.py
+++ services/index.py
@@ -1,8 +1,46 @@
 import json
+import boto3
+import os
+from uuid import uuid4
+
+table_name = os.environ.get('TABLE_NAME')
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(table_name)
+headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': '*',
+}
 
 def handler(event, context):
-  print("Event: ", event)
-  return {
-    "statusCode": 200,
-    "body": json.dumps("Hi"),
-  }
+  method = event['httpMethod']
+  body = event['body']
+
+  if method == 'POST':
+    item = json.loads(body)
+    item['id'] = str(uuid4())
+    table.put_item(Item=item)
+    return {
+      'statusCode': 200,
+      'body': json.dumps({'id': item['id']}),
+      'headers': headers
+    }
+    
+  
+  if method == 'GET':
+    empl_id = event['queryStringParameters']['id']
+
+    response = table.get_item(Key={
+      'id': empl_id
+    })
+    if 'Item' in response:
+      return {
+        'statusCode': 200,
+        'body': json.dumps(response['Item']),
+        'headers': headers
+      }
+    else:
+      return {
+        'statusCode': 404,
+        'body': json.dumps('Not Found'),
+        'headers': headers
+      }
